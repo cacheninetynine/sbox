782aa1b5797cc9e8ac8eabf11dd216cab85dc1f7
cacheninetynine <88933951+cacheninetynine@users.noreply.github.com>
2023-07-19 22:45:49 +0300

    use advanced ai technology to pick best key

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use uuid::Uuid;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
+    println!("Please wait, generating best key ever");
+    thread::sleep(Duration::from_millis(1000));
     let id = Uuid::new_v4();
     let uppercase_id = id.to_string().to_uppercase();
     println!("{}", uppercase_id);
