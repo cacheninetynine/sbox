7c2933bdbd10436c345b32d0fce9475b7f7bb457
cacheninetynine <mactimurik@gmail.com>
2023-07-20 20:23:14 +0300

    NEW UPDATE FOR INFINITE KEYS

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,9 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
-    println!("Please wait, generating best key ever");
-    thread::sleep(Duration::from_millis(1000));
-    println!("Using the blockchain advanced machine learning models edge computing iot quantum computing vr and cloud support to maximise the chance of a successful keygen");
-    thread::sleep(Duration::from_millis(2000));
-    println!("Mining bitcoin to keep our keygen servers running");
-    for f in 0..101 {
-        println!("Mined {}% of pro hacker bitcoin needed for 1 key",f);
-        thread::sleep(Duration::from_millis(452));
+    while 1 == 1{
+        let id = Uuid::new_v4();
+        let uppercase_id = id.to_string().to_uppercase();
+        println!("{}", uppercase_id);
     }
-    println!("Establishing connection to our servers...");
-    thread::sleep(Duration::from_millis(1200));
-    println!("Finishing, submitting your browser passwords to our server");
-    thread::sleep(Duration::from_millis(500));
-    println!("Thank you. You may have your key now.");
-    let id = Uuid::new_v4();
-    let uppercase_id = id.to_string().to_uppercase();
-    println!("{}", uppercase_id);
 }
